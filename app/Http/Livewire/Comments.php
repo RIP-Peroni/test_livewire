d3c1b6c4bc7355c387bf5fb269179126c168976e
Yuriy Epikhin <yepihin@tsargrad.tv>
2023-05-12 18:43:36 +0300

    minimum functionality provided

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
+    public $comments = [
+        [
+            'body' => 'lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi ex cupditatte quo commodi aspernatur delectus veniam necessitatibus.',
+            'created_at' => '3 min ago',
+            'creator' => 'Yuriy',
+        ]
+    ];
+
+    public $newComment;
+
+    public function addComment()
+    {
+        array_unshift($this->comments, 
+            [
+                'body' => $this->newComment,
+                'created_at' => Carbon::now()->diffForHumans(),
+                'creator' => 'Smb else',
+            ]
+        );
+    }
+
     public function render()
     {
         return view('livewire.comments');
